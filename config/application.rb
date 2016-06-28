92c8416bfd7a50cc021fe8bc5493215cbc70d4f1
Keisuke Ishizawa <k_ishizawa@sfidante.co.jp>
2016-06-28 21:57:16 +0900

    自動で出力されるspecファイルを制限

--- config/application.rb
+++ config/application.rb
@@ -31,5 +31,13 @@ module Kiita
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    config.generators do |g|
+      g.test_framework :rspec,
+        view_specs: false,
+        routing_specs: false,
+        helper_specs: false,
+        controller_specs: false
+    end
   end
 end
