271855ab09e4019c0de9d031db578a863ac886ad
Keisuke Ishizawa <k_ishizawa@sfidante.co.jp>
2016-07-04 22:21:31 +0900

    markdown のオプションに fenced_code_blocks を追加

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
   def markdown(body)
     renderer = Redcarpet::Render::HTML.new(filter_html: true)
-    markdown = Redcarpet::Markdown.new(renderer,  tables: true, autolink: true)
+    markdown = Redcarpet::Markdown.new(renderer,  tables: true, autolink: true, fenced_code_blocks: true)
     markdown.render(body).html_safe
   end
 end
